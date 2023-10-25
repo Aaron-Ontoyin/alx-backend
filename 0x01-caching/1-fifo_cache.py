a1f21e272d392a1de2e3d5d1ff71322a1162a74d
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-25 21:11:04 +0000

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python3
 """FIFO caching"""
-from collections import OrderedDict
-BaseCaching = __import__('base_caching').BaseCaching
-
+from collections import deque
+from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
     """FIFO caching class"""
 
-    # def __init__(self):
-    #     """Initialize"""
-    #     super().__init__()
-    #     self.queue = OrderedDict()
+    def __init__(self):
+        """Initialize the FIFO cache."""
+        super().__init__()
+        self.queue = deque()
+    
+    def evict(self):
+        """Evict the first item in the cache (FIFO)."""
+        if self.queue and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_in = self.queue.popleft()
+            print(f"DISCARD: {first_in}")
+            del self.cache_data[first_in]
 
     def put(self, key, item):
-        """Add an item in the cache"""
+        """Add an item in the cache."""
         if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                if key not in self.cache_data:
-                    first_in = self.cache_data.keys()[-1]
-                    del self.cache_data[first_in]
-                    print(f"DISCARD: {first_in}")
-                    # popped = self.queue.popitem(last=False)
-                    # print(f"DISCARD: {popped[0]}")
-                    # del self.cache_data[popped[0]]
-            # self.cache_data[key] = item
-            # self.queue[key] = 1
+            self.evict()
+            self.cache_data[key] = item
+            self.queue.append(key)
 
     def get(self, key):
-        """Get the associated value of key"""
+        """Get the associated value of key."""
         return self.cache_data.get(key)

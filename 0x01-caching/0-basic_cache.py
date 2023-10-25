0cd856ab3d25e281a5f4976fed068ffef333a810
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-25 20:41:26 +0000

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,7 +8,7 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """Inserts a new cached data into the cache"""
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):

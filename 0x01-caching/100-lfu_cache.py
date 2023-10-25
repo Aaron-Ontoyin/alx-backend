5c621b1253c58540282f227aeaad22bbf40e0778
Aaron-Ontoyin <aarononto90@gmail.com>
2023-10-25 23:27:44 +0000

    100

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -14,26 +14,28 @@ class LFUCache(BaseCaching):
     def evict(self):
         """Evict the least frequently used item in the cache (LFU)."""
         if self.lfu and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            lfu = sorted(self.lfu.items(), key=lambda x: x[1])[0][0]
-            del self.cache_data[lfu]
-            print(f"DISCARD: {lfu}")
+            lfu_key = sorted(self.lfu.items(), key=lambda x: x[1])[0][0]
+            del self.cache_data[lfu_key]
+            del self.lfu[lfu_key]
+            print(f"DISCARD: {lfu_key}")
 
     def put(self, key, item):
         """Add an item in the cache."""
-        if key is not None and item is not None:
-            if self.lfu.get(key):
-                self.lfu[key] += 1
-            else:
-                self.lfu[key] = 1
-            self.evict()
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.lfu[key] += 1
             self.cache_data[key] = item
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                self.evict()
+            self.cache_data[key] = item
+            self.lfu[key] = 1
 
     def get(self, key):
         """Get the associated value of key."""
         value = self.cache_data.get(key)
         if value:
-            if self.lfu.get(key):
-                self.lfu[key] += 1
-            else:
-                self.lfu[key] = 1
+            self.lfu[key] += 1
         return value
